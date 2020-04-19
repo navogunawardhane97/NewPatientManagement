42443d486f53574403d409846dacd8807d7ad99e
navogunawardhane97 <61610146+navogunawardhane97@users.noreply.github.com>
2020-04-19 05:35:00 +0530

    First commit

---
+++ NewPatient/src/main/java/com/project/newpatient/NewPatient/PatientRepository.java
@@ -0,0 +1,161 @@
+package com.project.newpatient.NewPatient;
+/*import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;*/
+
+public class PatientRepository {
+/*Connection con = null;
+	
+	
+	public PatientRepository() {
+		
+		try {
+			
+				// Connect to Database
+				Class.forName("com.mysql.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3308/patientdatabase","root","");
+				System.out.println("Database Connected....");
+		}
+		catch(Exception e) {
+			
+			System.out.println(e);
+		}
+	}
+
+	public List<Patient> getPatient() {
+		
+		List<Patient> patient = new ArrayList<>(); 
+		String sql = "select * from patientpatientdatabase"; 
+		
+		try {
+			
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			while(rs.next()) {
+				
+				Patient p = new Patient();
+				
+				p.setPid(rs.getInt(1));
+				p.setNic(rs.getString(2));
+				p.setFname(rs.getString(3));
+				p.setLname(rs.getString(4));
+				p.setAge(rs.getInt(5));
+				p.setAddress(rs.getString(6));
+				p.setGender(rs.getString(7));
+				p.setEmail(rs.getString(8));
+				p.setPhoneNo(rs.getInt(9));
+				
+				patient.add(p);
+			}
+		}
+		catch(Exception e) {
+			
+			System.out.println(e);
+		}
+		return patient;
+	}
+
+	public Patient getPatient(int pid) {
+		
+		String sql = "select * from patient where pid = "+pid;
+		Patient p = new Patient();
+		try 
+		{
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		if(rs.next()) 
+		{
+	
+			p.setPid(rs.getInt(1));
+			p.setNic(rs.getString(2));
+			p.setFname(rs.getString(3));
+			p.setLname(rs.getString(4));
+			p.setAge(rs.getInt(5));
+			p.setAddress(rs.getString(6));
+			p.setGender(rs.getString(7));
+			p.setEmail(rs.getString(8));
+			p.setPhoneNo(rs.getInt(9));
+			
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return p;
+	}
+
+	public void create(Patient a1) {
+		String sql = "insert into patient values(?,?,?,?,?,?,?,?,?)";
+		try 
+		{
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, a1.getPid());
+		st.setString(2, a1.getNic());
+		st.setString(3, a1.getFname());
+		st.setString(4, a1.getLname());
+		st.setInt(5, a1.getAge());
+		st.setString(6, a1.getAddress());
+		st.setString(7, a1.getGender());
+		st.setString(8, a1.getEmail());
+		st.setInt(9, a1.getPhoneNo());
+		st.executeUpdate();
+		
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
+
+	
+	public void update(Patient a1) {
+		
+		String sql = "update patient set nic=?,fname=?,lname=?,age=?,address=?,gender=?,email=?,phoneNo=? where pid=?";
+		try 
+		{
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, a1.getNic());
+		st.setString(2, a1.getFname());
+		st.setString(3, a1.getLname());
+		st.setInt(4, a1.getAge());
+		st.setString(5, a1.getAddress());
+		st.setString(6, a1.getGender());
+		st.setString(7, a1.getEmail());
+		st.setInt(8, a1.getPhoneNo());
+		st.setInt(9, a1.getPid());
+
+		st.executeUpdate();
+		
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
+
+	public void delete(int pid) {
+		String sql = "delete from patient where pid=?";
+		try 
+		{
+		PreparedStatement st = con.prepareStatement(sql);
+		
+		st.setInt(1, pid);
+
+
+		st.executeUpdate();
+		
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+}*/
+}
---
+++ NewPatient/src/main/java/com/project/newpatient/NewPatient/PatientResource.java
@@ -0,0 +1,83 @@
+package com.project.newpatient.NewPatient;
+/*import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("patients")*/
+public class PatientResource {
+	/*PatientRepository repo = new PatientRepository();
+
+	@GET
+	
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML}) 
+	public List<Patient> getPatient() {  
+		
+		
+		System.out.println("Patient calling...");
+		return repo.getPatient();
+	}
+	
+	@GET
+	@Path("patient/{pid}")
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	
+	public Patient getPatient(@PathParam("pid") int pid) {
+		
+		System.out.println("Successful");
+		return repo.getPatient(pid);
+	}
+
+	@POST
+	@Path("patient")
+	@Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public Patient CreatePatient(Patient a1) {
+		System.out.println(a1);
+		repo.create(a1);
+		
+		return a1;
+	}
+	
+	
+	@PUT
+	@Path("patient")
+	@Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public Patient UpdatePatient(Patient a1) {
+		
+		System.out.println(a1);
+		
+		if(repo.getPatient(a1.getPid()).getPid()==0) {
+			
+			repo.create(a1);
+		}
+		else
+		{ 
+			
+			repo.update(a1);
+			System.out.println("Update patient Successfully...");
+		}
+		
+		return a1;
+	}
+	
+	@DELETE
+	@Path("patient/{pid}")
+	public Patient removePatient(@PathParam("pid")int pid)
+	{
+		Patient a = repo.getPatient(pid);
+		if(a.getPid()!= 0) {
+			
+			repo.delete(pid);
+		}
+		
+		return a;
+	}*/
+	
+}
